4a6a77bb7b903fb2cbc04e16358b9e614ab0cf3a
Tomivan <gracejunkie20@gmail.com>
2021-10-12 10:39:49 +0100

    fixed raised issue

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Error404Page } from "tabler-react";
+import Link from 'react-router-dom';
 import SiteWrapper from "./SiteWrapper";
 
 class NoMatch extends Component {
   render() {
     return (
       <SiteWrapper>
+        <Link to="/login">Login</Link>
         <Error404Page />
       </SiteWrapper>
     );
